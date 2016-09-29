7629c406644539662483f77953f0c3b2363a6aa1
seonggyeongkim <skim7902@gamil.com>
2016-09-29 19:10:53 +0900

    #2 Add String and Change Layout

--- app/src/main/java/kr/hs/emirim/duckbill7902/touchcraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/duckbill7902/touchcraphic/MainActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.duckbill7902.touchcraphic;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,8 +27,14 @@ public class MainActivity extends AppCompatActivity {
                 float cx=getWidth()/2.0f;
                 float cy=getHeight()/2.0f;
                 Paint paint=new Paint(); //굵기 설정
+                paint.setStrokeWidth(4);
+                paint.setColor(Color.BLUE);
+                paint.setStyle(Paint.Style.STROKE);//빈 원 모양!
 
-                canvas.drawCircle(cx,cy,50,paint);
+                canvas.drawCircle(cx,cy,200,paint);
+                paint.setColor(Color.GREEN);
+                paint.setStyle(Paint.Style.FILL);//채워진 사각형 모양!
+                canvas.drawRect(100,100,300,250,paint);
         }
     }
 }
